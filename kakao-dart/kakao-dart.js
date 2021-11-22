6efc70810cc98d15ac5918b59d4f3ce5d8ccb7a7
MiranLee <71805803+MiranLeeeee@users.noreply.github.com>
2021-11-22 17:31:15 +0900

    calcDart function 조건제어문 변경

--- kakao-dart/kakao-dart.js
+++ kakao-dart/kakao-dart.js
@@ -1,49 +1,36 @@
 function calcDart(score, bonus) {
-    let result = score;
-
-    switch(bonus){
-        case 'D': 
-            result = Math.pow(score, 2);
-            break;
-        case 'T':
-            result = Math.pow(score, 3);
-            break;
-        default:
-             break;
-    }
-    return result;
+  return bonus === "D"
+    ? Math.pow(score, 2)
+    : bonus === "T"
+    ? Math.pow(score, 3)
+    : score;
 }
 
 function solution(dartResult) {
-    let result = 0;
-    let answer = 0;
-    let score = 0;
-    let scores = [];
-    
-    for (let i =0; i<dartResult.length; i++) {
-        if(dartResult[i] === '0'){
-            score = dartResult[i-1] && dartResult[i-1] === '1'? 10 : 0;
+  let result = 0;
+  let answer = 0;
+  let score = 0;
+  let scores = [];
+
+  for (let i = 0; i < dartResult.length; i++) {
+    if (dartResult[i] === "0") {
+      score = dartResult[i - 1] && dartResult[i - 1] === "1" ? 10 : 0;
+    } else if (Number(dartResult[i])) {
+      score = Number(dartResult[i]);
+    } else {
+      if (dartResult[i] === "*") {
+        if (scores[scores.length - 2]) {
+          scores[scores.length - 2] *= 2;
         }
-        else if(Number(dartResult[i])){
-            score = Number(dartResult[i]);
-        }
-        else {
-            
-            if(dartResult[i] === '*'){
-               if (scores[scores.length-2]){
-                    scores[scores.length-2] *= 2;
-               }
-               scores[scores.length-1] *= 2; 
-                
-            }else if (dartResult[i] === '#') {
-                scores[scores.length-1] *= (-1);
-            }else{               
-                answer = calcDart(score, dartResult[i]);
-                scores.push(answer);
-            }   
-            
-        }
+        scores[scores.length - 1] *= 2;
+      } else if (dartResult[i] === "#") {
+        scores[scores.length - 1] *= -1;
+      } else {
+        answer = calcDart(score, dartResult[i]);
+        scores.push(answer);
+      }
     }
-    scores.forEach(num => result+=num)
-    return result;
+  }
+  scores.forEach((num) => (result += num));
+  return result;
 }
