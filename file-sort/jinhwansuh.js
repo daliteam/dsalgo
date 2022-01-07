04285593bdbc8d494841aa1f7c8ae57e32088c41
jinhwansuh <nick02190219@gmail.com>
2022-01-07 15:07:34 +0900

    [진환] 파일명 정렬 정규표현식 사용

--- file-sort/jinhwansuh.js
+++ file-sort/jinhwansuh.js
@@ -1,72 +1,18 @@
-const sortFiles = (files) => {
-  const array = [];
-
-  for (const file of files) {
-    let head = '',
-      number = '';
-    const tempArray = [];
-    for (let i = 0; i < file.length; i++) {
-      if (+file[i] + 1) {
-        number += file[i];
-        if (file[i + 1] !== ' ' && +file[i + 1] + 1) {
-          // 다음이 공백이 아니고 숫자이면.
-          if (number.length === 5) {
-            tempArray.push(number);
-            tempArray.push(file.slice(i + 1));
-            break;
-          }
-          continue;
-        } else {
-          // 다음이 숫자가 아니라면
-          tempArray.push(number);
-          tempArray.push(file.slice(i + 1));
-          break;
-        }
-      } else {
-        head += file[i];
-        if (+file[i + 1] + 1) {
-          // 다음이 숫자이면 push
-          tempArray.push(head);
-        }
-      }
-    }
-    array.push(tempArray);
-  }
-  return array;
-};
-
 function solution(files) {
-  const sortedArray = sortFiles(files);
-  // sortedArray는 아래처럼 나온다.
+  const answer = [...files];
 
-  sortedArray.sort((a, b) => {
-    if (a[0].toLowerCase() === b[0].toLowerCase()) {
-      const x = +a[1],
-        y = +b[1];
-      return x < y ? -1 : x > y ? 1 : 0;
-    }
-    return a[0].toLowerCase() < b[0].toLowerCase() ? -1 : 1;
+  answer.sort((a, b) => {
+    const headA = a.match(/^\D+/)[0].toLowerCase();
+    const headB = b.match(/^\D+/)[0].toLowerCase();
+
+    const numberA = parseInt(a.match(/\d+/)[0], 10);
+    const numberB = parseInt(b.match(/\d+/)[0], 10);
+
+    if (headA < headB) return -1;
+    if (headA > headB) return 1;
+
+    return numberA - numberB;
   });
 
-  return sortedArray.map((array) => array.join(''));
+  return answer;
 }
-
-/* 
-[
-  [ 'img', '12', '.png' ],
-  [ 'img', '10', '.png' ],
-  [ 'img', '02', '.png' ],
-  [ 'img', '1', '.png' ],
-  [ 'IMG', '01', '.GIF' ],
-  [ 'img', '2', '.JPG' ]
-] 
-*/
-
-/* 
-[
-  [ 'F-', '5', ' Freedom Fighter' ],
-  [ 'B-', '50', ' Superfortress' ],
-  [ 'A-', '10', ' Thunderbolt II' ],
-  [ 'F-', '14', ' Tomcat' ]
-]
-*/
