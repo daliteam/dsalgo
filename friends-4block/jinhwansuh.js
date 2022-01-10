7877b54308b43aa1619d9e80089a03385fca4872
jinhwansuh <nick02190219@gmail.com>
2022-01-10 16:16:22 +0900

    [진환] 프렌즈 4블록

--- friends-4block/jinhwansuh.js
+++ friends-4block/jinhwansuh.js
@@ -1,25 +1,19 @@
-const changeStringtoArray = (board) => {
-  const changedArray = [];
-  board.map((board) => {
-    changedArray.push(board.split(''));
-  });
-  return changedArray;
-};
-
 const replaceBoard = (m, n, boardArray) => {
   const sameStringArray = [];
   for (let i = 0; i < m; i++) {
     for (let j = 0; j < n; j++) {
       let x = i,
         y = j;
-      const a = boardArray[i][j];
-      if (x + 1 < m && y + 1 < n) {
-        if (
-          boardArray[i + 1][j] === a &&
-          boardArray[i][j + 1] === a &&
-          boardArray[i + 1][j + 1] === a
-        ) {
-          sameStringArray.push([i, j]);
+      if (boardArray[i][j]) {
+        const string = boardArray[i][j];
+        if (x + 1 < m && y + 1 < n) {
+          if (
+            boardArray[i + 1][j] === string &&
+            boardArray[i][j + 1] === string &&
+            boardArray[i + 1][j + 1] === string
+          ) {
+            sameStringArray.push([i, j]);
+          }
         }
       }
     }
@@ -30,41 +24,42 @@ const replaceBoard = (m, n, boardArray) => {
     boardArray[a][b + 1] = '';
     boardArray[a + 1][b + 1] = '';
   });
-  return boardArray;
+  return [boardArray, sameStringArray];
 };
 
 const moveToEmptyBottom = (m, n, array) => {
-  let isTrue = false;
-  for (let i = 0; i < m; i++) {
+  for (let i = m - 1; i > 0; i--) {
     for (let j = 0; j < n; j++) {
-      let x = i,
-        y = j;
-      if (x + 1 < m && y + 1 < n) {
-        if (array[x][y] && array[x + 1][y] === '') {
-          [array[x][y], array[x + 1][y]] = [array[x + 1][y], array[x][y]];
-          isTrue = true;
+      if (array[i][j] !== '') {
+        continue;
+      }
+      for (let k = i - 1; k > -1; k--) {
+        if (array[k][j] !== '') {
+          [array[i][j], array[k][j]] = [array[k][j], array[i][j]];
+          break;
         }
       }
     }
   }
-  return [isTrue, array];
+  return array;
 };
 
 function solution(m, n, board) {
-  const boardArray = changeStringtoArray(board);
-  const array = replaceBoard(m, n, boardArray);
-  let isEmpty = true,
-    newArray = array;
+  let boardArray = [...board.map((v) => [...v])];
 
-  while (isEmpty) {
-    const [empty1, array1] = moveToEmptyBottom(m, n, newArray);
-    newArray = replaceBoard(m, n, array1);
-    const [empty2, array2] = moveToEmptyBottom(m, n, newArray);
-    if (!empty2) isEmpty = false;
+  let indexArray = [],
+    boardArray1,
+    array1;
+
+  while (true) {
+    [boardArray1, indexArray] = replaceBoard(m, n, boardArray);
+    if (indexArray.length === 0) break;
+    array1 = moveToEmptyBottom(m, n, boardArray1);
+    boardArray = [...array1];
   }
 
   let answer = 0;
-  newArray.map((array) =>
+  boardArray.map((array) =>
     array.map((string) => {
       if (!string) answer++;
     })
