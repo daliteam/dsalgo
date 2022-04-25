d79b705991feb911b7ef0f86dfaec280e318e797
jinhwansuh <nick02190219@gmail.com>
2022-04-25 13:03:51 +0900

    03 해결

--- practice/jinhwansuh/0425/03.js
+++ practice/jinhwansuh/0425/03.js
@@ -1,8 +1,7 @@
-// 불량 사용자
+// 불량사용자
 
 function solution(user_id, banned_id) {
   const answer = [];
-
   const a = [];
 
   for (const banId of banned_id) {
@@ -22,22 +21,38 @@ function solution(user_id, banned_id) {
         if (s === userId) temp.push(s);
       }
     }
-    a.push([banId, temp]);
+    a.push(temp);
   }
-  console.log(a);
-
-  const allCombinations = [];
-  let index = 0;
 
   // 도움!
+  // 완전탐색
   // 아래 배열에서 1개씩 뽑는다.
   // 정렬해서 더한다.
+  // set
   // 더한걸 정렬한다.
   // 마지막 set으로 갯수를 센다 (중복제거);
 
-  for (let i = 0; i < 4; i++) {
-    for (let j = 0; i < 4; j++) {}
-  }
+  // 재귀를 돈다.
 
-  return answer;
+  const sets = new Set();
+  const d = [];
+  const recur = (list, set) => {
+    if (list.length === 0) {
+      const result = [...set].sort().join('');
+      sets.add(result);
+      return;
+    }
+    const restList = list.slice(1);
+
+    list[0].forEach((id) => {
+      const copy = new Set([...set]);
+      if (copy.has(id)) return;
+      copy.add(id);
+      recur(restList, copy);
+    });
+  };
+
+  recur(a, new Set());
+
+  return sets.size;
 }
