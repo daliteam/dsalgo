18ebb43fea66f413adc9d947028344a69ea583ba
jinhwansuh <nick02190219@gmail.com>
2022-06-11 16:30:36 +0900

    백준 11727  DP

--- practice/jinhwansuh/0611/01.js
+++ practice/jinhwansuh/0611/01.js
@@ -1,19 +1,37 @@
 // 백준 11726
 
 {
-  const a = [0, 1, 2];
+  const array = [0, 1, 2];
 
   function dp(number) {
     if (number === 1) return 1;
     if (number === 2) return 2;
 
-    if (a[number]) return a[number];
+    if (array[number]) return array[number];
     else {
-      a[number] = dp(number - 1) + dp(number - 2);
-      return a[number];
+      array[number] = dp(number - 1) + dp(number - 2);
+      return array[number];
     }
   }
-  console.log(a[55]);
+
+  console.log(dp(2));
+  console.log(dp(9));
+}
+
+// 백준 11727
+{
+  const array = [0, 1, 2];
+
+  function dp(number) {
+    if (number === 1) return 1;
+    if (number === 2) return 2;
+
+    if (array[number]) return array[number];
+    else {
+      array[number] = dp(number - 1) + 2 * dp(number - 2);
+      return array[number];
+    }
+  }
 
   console.log(dp(2));
   console.log(dp(9));
