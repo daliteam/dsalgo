65ad550a23e7c9904730763cf3c9cd0e7b8be69f
jinhwansuh <nick02190219@gmail.com>
2022-06-11 16:35:30 +0900

    백준 2133 DP

--- practice/jinhwansuh/0611/01.js
+++ practice/jinhwansuh/0611/01.js
@@ -36,3 +36,29 @@
   console.log(dp(2));
   console.log(dp(9));
 }
+
+// 백준 2133
+{
+  const array = [1, 0, 3];
+
+  function dp(number) {
+    if (number === 0) return 1;
+    if (number === 1) return 0;
+    if (number === 2) return 3;
+
+    if (array[number]) return array[number];
+
+    let result = 3 * dp(number - 2);
+
+    for (let i = 3; i <= number.length; i++) {
+      if (i % 2 === 0) {
+        result += 2 * dp(x - i);
+      }
+    }
+    array[number] = result;
+    return array[number];
+  }
+
+  console.log(dp(2));
+  console.log(dp(10));
+}
